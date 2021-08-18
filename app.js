eafb0cf68ede9dc9fb18a8614a183d745ddb6912
sebasla76 <sebasla76@hotmail.com>
2021-08-18 18:47:34 -0300

    mercadoLiebre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.listen(3000,()=> console.log("servidor corriendo"));
+const publicPath=path.resolve(__dirname,"./public");
+app.use(express.static(publicPath));
+app.get('/' ,(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/indexcopy.html")
+    )});
+app.get('/register.html' ,(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/register.html")
+     )});    
+app.get('/login.html' ,(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/login.html")
+     )});         
